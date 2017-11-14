d798c343646d0797a4665feea7566de1af254c4d
Mehdi Bouaziz <Alb4t4r@users.noreply.github.com>
2017-11-14 15:38:50 +0100

    CAD portfolio

--- js/index.js
+++ js/index.js
@@ -5,3 +5,47 @@ $("#navbtn").click(function(){
 $("#navdrop").click(function(){
     $("#navdrop").slideUp();
 });
+
+
+function overon1() {
+    document.getElementById("autocad1").style.display = "block";
+}
+function overoff1() {
+    document.getElementById("autocad1").style.display = "none";
+} 
+
+
+
+function overon2() {
+    document.getElementById("autocad2").style.display = "block";
+}
+function overoff2() {
+    document.getElementById("autocad2").style.display = "none";
+} 
+
+
+
+function overon3() {
+    document.getElementById("autocad3").style.display = "block";
+}
+function overoff3() {
+    document.getElementById("autocad3").style.display = "none";
+} 
+
+
+
+function overon4() {
+    document.getElementById("autocad4").style.display = "block";
+}
+function overoff4() {
+    document.getElementById("autocad4").style.display = "none";
+} 
+
+
+
+function overon5() {
+    document.getElementById("autocad5").style.display = "block";
+}
+function overoff5() {
+    document.getElementById("autocad5").style.display = "none";
+}
